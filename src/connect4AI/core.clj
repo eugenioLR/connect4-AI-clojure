c1e85b678a506f5281270b9374338dcf578d93c3
eugeniolr <eugeniolorente0@gmail.com>
2021-01-01 21:21:19 +0100

    better UI

--- src/connect4AI/core.clj
+++ src/connect4AI/core.clj
@@ -3,7 +3,17 @@
                     (:gen-class))
 
 (defn -main [& args]
-    (prn (connect4-game human-move #(connect4-AI-move %1 %2 %3 1) true)))
+    (if (> (count args) 0)
+        (try
+            (let [level (Integer. (first args))]
+                 (prn level)
+                 (if (> level 0)
+                     (prn (connect4-game human-move #(connect4-AI-move %1 %2 %3 level) true))
+                     (prn (connect4-game human-move random-move true))))
+        (catch NumberFormatException n
+            (prn "The AI level must be a number.")))
+        (prn (connect4-game human-move #(connect4-AI-move %1 %2 %3 1) true))))
+
 
 ;(prn (connect4-game #(connect4-AI-move %1 %2 %3 3) #(connect4-AI-move %1 %2 %3 3) true))
 ;(prn (/ (count (for [i (range 100) :when (= (get (connect4-game random-move #(connect4-AI-move %1 %2 %3 4) false) :winner) 1)] i)) 100.0))
